5bddc467c5b91abf84392fe3ac042a6450c3fd3c
Nikolaos Chatzikonstantinou <nchatz314@gmail.com>
2025-01-02 17:36:17 -0500

    add cli command

---
+++ rfc_browser/cli.py
@@ -0,0 +1,49 @@
+from . import load_xml
+import argparse
+
+
+def parse_rfc_name(s):
+    s = s.upper()
+    if s.startswith("RFC"):
+        s = s[3:]
+    if not s.isdigit():
+        return None
+    return "RFC" + s
+
+
+class ParseRFCName(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        super().__init__(option_strings, dest, nargs, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        del option_string
+        acc = [parse_rfc_name(v) for v in values]
+        if not all(acc):
+            raise argparse.ArgumentError(self, f"An argument does not refer to an RFC.")
+        setattr(namespace, self.dest, acc)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="rfc-browser",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="""An RFC browser that generates SVG maps.
+
+Prints a graph in DOT language format.
+""",
+        epilog="More on <https://github.com/createyourpersonalaccount/rfc_browser>.",
+    )
+    parser.add_argument(
+        "RFC",
+        nargs="+",
+        action=ParseRFCName,
+        help="Should be given in the format RFCN (or just N); e.g. RFC1234.",
+    )
+    parser.add_argument(
+        "-f",
+        "--full",
+        action="store_true",
+        help="The full graph instead of the transitive reduction.",
+    )
+    args = parser.parse_args()
+    load_xml.produce_svg(args.RFC, args.full)
--- rfc_browser/load_xml.py
+++ rfc_browser/load_xml.py
@@ -1,31 +1,49 @@
+from importlib.resources import files
 import xml.etree.ElementTree as ET
 import networkx as nx
+import networkx.readwrite.json_graph as json_graph
+import json
+import sys
 
-tree = ET.parse("../rfc-index.xml")
-root = tree.getroot()
 ns = {"": "https://www.rfc-editor.org/rfc-index"}
+rfcs = {}
+nodes = nx.DiGraph()
+ug = nx.DiGraph()
+ubg = nx.DiGraph()
+og = nx.DiGraph()
+obg = nx.DiGraph()
+
 
 class RFC:
-    def __init__(self, el):
-        self.id = el.find('doc-id', ns).text
-        self.title = child.find('title', ns).text
-        self.date = child.find('date', ns).find('year', ns).text
-        self.current_status = child.find('current-status', ns).text
+    def __init__(self, child):
+        self.id = child.find("doc-id", ns).text
+        self.title = child.find("title", ns).text
+        self.date = child.find("date", ns).find("year", ns).text
+        self.current_status = child.find("current-status", ns).text
         # Have to filter these to only contain RFCs.
         self.updates = [
-            update.text for update in child.find('updates', ns) or [] if update.text.startswith("RFC")
+            update.text
+            for update in child.find("updates", ns) or []
+            if update.text.startswith("RFC")
         ]
         self.updated_by = [
-            updated.text for updated in child.find('updated-by', ns) or [] if updated.text.startswith("RFC")
+            updated.text
+            for updated in child.find("updated-by", ns) or []
+            if updated.text.startswith("RFC")
         ]
         self.obsoletes = [
-            obsolete.text for obsolete in child.find('obsoletes', ns) or [] if obsolete.text.startswith("RFC")
+            obsolete.text
+            for obsolete in child.find("obsoletes", ns) or []
+            if obsolete.text.startswith("RFC")
         ]
         self.obsoleted_by = [
-            obsoleted.text for obsoleted in child.find('obsoleted-by', ns) or [] if obsoleted.text.startswith("RFC")
+            obsoleted.text
+            for obsoleted in child.find("obsoleted-by", ns) or []
+            if obsoleted.text.startswith("RFC")
         ]
-        self.stream = child.find('stream', ns).text
-        self.doi = child.find('doi', ns).text
+        self.stream = child.find("stream", ns).text
+        self.doi = child.find("doi", ns).text
+
 
 def style_graph(a):
     for node in a.nodes():
@@ -34,10 +52,10 @@ def style_graph(a):
             case "INTERNET STANDARD":
                 node.attr["shape"] = "box"
                 node.attr["label"] = f"{node}\nSTD"
-                node.attr["fillcolor"] = "cornflowerblue"
+                node.attr["fillcolor"] = "darkorchid1"
                 node.attr["style"] = "filled"
             case "DRAFT STANDARD":
-                node.attr["shape"] = "double circle"
+                node.attr["shape"] = "doublecircle"
                 node.attr["label"] = f"{node}\nDRAFT"
             case "PROPOSED STANDARD":
                 node.attr["shape"] = "ellipse"
@@ -68,67 +86,85 @@ def style_graph(a):
         if obg.has_edge(*edge):
             edge.attr["style"] = "dotted"
 
-rfcs = {}
 
-for child in root.findall('rfc-entry', ns):
-    rfc = RFC(child)
-    rfcs[rfc.id] = rfc
+def print_graph(g):
+    a = nx.nx_agraph.to_agraph(g)
+    style_graph(a)
+    a.write(sys.stdout)
 
-nodes = nx.DiGraph()
-ug = nx.DiGraph()
-ubg = nx.DiGraph()
-og = nx.DiGraph()
-obg = nx.DiGraph()
 
-for rfc in rfcs.values():
-    nodes.add_node(rfc.id)
+def write_cyjs(g, filename):
+    data = json_graph.cytoscape_data(g)
+    with open(filename, "w") as f:
+        json.dump(data, f)
 
-ug.add_nodes_from(nodes)
-ubg.add_nodes_from(nodes)
-og.add_nodes_from(nodes)
-obg.add_nodes_from(nodes)
 
-for rfc in rfcs.values():
-    for k in rfc.updates:
-        ug.add_edge(rfc.id, k)
-    for k in rfc.updated_by:
-        ubg.add_edge(rfc.id, k)
-    for k in rfc.obsoletes:
-        og.add_edge(rfc.id, k)
-    for k in rfc.obsoleted_by:
-        obg.add_edge(rfc.id, k)
+def produce_svg(rfc_list, full):
+    rfc_index = files("rfc_browser").joinpath("rfc-index.xml")
+    tree = ET.parse(rfc_index)
+    root = tree.getroot()
 
-# Find the descendants of RFC1034, RFC1035, and RFC1886
-c = nx.compose(ubg, obg)
-l = []
-for r in ["RFC1034", "RFC1035", "RFC1886"]:
-    x = nx.descendants(c, r)
-    x.add(r)
-    l.append(nx.subgraph(c, x))
-# Limit the graph to only those RFCs and their descendants
-g = nx.compose_all(l)
-a = nx.nx_agraph.to_agraph(g)
-style_graph(a)
-a.write("before_reduction.gv")
+    for child in root.findall("rfc-entry", ns):
+        rfc = RFC(child)
+        rfcs[rfc.id] = rfc
 
-t = nx.transitive_reduction(g)
-a = nx.nx_agraph.to_agraph(nx.intersection(g, t))
-style_graph(a)
-a.write("with_obsolete.gv")
+    for rfc in rfcs.values():
+        nodes.add_node(rfc.id)
 
-g_without = nx.DiGraph()
+    ug.add_nodes_from(nodes)
+    ubg.add_nodes_from(nodes)
+    og.add_nodes_from(nodes)
+    obg.add_nodes_from(nodes)
 
-for u, v in g.edges():
-    if obg.has_edge(u, v):
-        for x in nx.ancestors(g, u):
-            g_without.add_edge(x, v)
-    else:
-        g_without.add_edge(u, v)
+    for rfc in rfcs.values():
+        for k in rfc.updates:
+            ug.add_edge(rfc.id, k)
+        for k in rfc.updated_by:
+            ubg.add_edge(rfc.id, k)
+        for k in rfc.obsoletes:
+            og.add_edge(rfc.id, k)
+        for k in rfc.obsoleted_by:
+            obg.add_edge(rfc.id, k)
 
-# TODO now we're supposed to transitively reduce but we haven't gotten
-# the edge contraction down yet... FIXME.
-# t = nx.transitive_reduction(g_without)
-# a = nx.nx_agraph.to_agraph(nx.intersection(g_without, t))
-a = nx.nx_agraph.to_agraph(g_without)
-style_graph(a)
-a.write("without_obsolete.gv")
+    # Find the descendants of RFC1034 and RFC1035
+    # Note that RFC1886 can be thrown into the mix.
+    c = nx.compose(ubg, obg)
+    l = []
+    for r in rfc_list:
+        x = nx.descendants(c, r)
+        x.add(r)
+        l.append(nx.subgraph(c, x))
+    # Limit the graph to only those RFCs and their descendants
+    g = nx.compose_all(l)
+
+    # Write the full graph before any reductions
+    if full:
+        print_graph(g)
+        return
+    # write_cyjs(g, "before_reduction.cyjs")
+
+    # Reduce the full graph into its transitive reduction
+    t = nx.transitive_reduction(g)
+    g_reduced = nx.intersection(g, t)
+    print_graph(g_reduced)
+
+    # Work hard to get rid of obsolete nodes. It should be a simple edge
+    # contraction but it has certain complications I haven't quite yet
+    # figured out, which means the code below isn't as simple as it could
+    # be.  For example, 1886 gets disposed of even though we'd like to
+    # keep it, as the user requested it as a root node.
+    # h = g.reverse()
+    # obg_restrict = nx.intersection(g, obg)
+    # for u, v in obg_restrict.edges():
+    #     try:
+    #         nx.contracted_edge(h, (v, u), self_loops=False, copy=False)
+    #     except:
+    #         continue
+    # h = h.reverse(copy=False)
+    # h_noloop = h.copy()
+    # for u, v in h.edges():
+    #     if nx.has_path(g, v, u):
+    #         h_noloop.remove_edge(u, v)
+    # t = nx.transitive_reduction(h_noloop)
+    # g_contracted = nx.intersection(h_noloop, t)
+    # print_graph(g_contracted, "without_obsolete.gv")
